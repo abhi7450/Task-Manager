eb2660b7d7f6e0c0b7f5331c42a1c44f7b60578c
= <=>
2022-06-05 11:46:26 +0530

    added model and the mongoose feature for CRUD

--- controllers/tasks.controller.js
+++ controllers/tasks.controller.js
@@ -1,16 +1,42 @@
-const Task = require("../models/Tasks")
+const Task = require("../models/Tasks.model")
 
-exports.getAllTasks = (req, res) => {
-    res.send("get all the task")
+exports.getAllTasks = async (req, res) => {
+    try {
+        const tasks = await Task.find({})
+        res.status(201).json({ tasks })
+    } catch (error) {
+        res.status(500).json({
+            message: error,
+        })
+    }
 }
 
 exports.createTask = async (req, res) => {
-    const task = await Task.create(req.body)
-    res.status(201).json({ task })
+    try {
+        const task = await Task.create(req.body)
+        res.status(201).json({ task })
+    } catch (error) {
+        res.status(500).json({
+            message: error,
+        })
+    }
 }
 
-exports.getTasks = (req, res) => {
-    res.json({ id: req.params.id })
+exports.getTasks = async (req, res) => {
+    try {
+        const { id: taskId } = req.params
+        const task = await Task.findOne({ _id: taskId })
+        if (!task) {
+            return res
+                .status(404)
+                .json({ message: `No task with id ${taskId}` })
+        }
+        res.status(201).json({ task })
+    } catch (error) {
+        res.status(500).json({
+            message: error,
+        })
+    }
 }
 
 exports.updateTask = (req, res) => {
